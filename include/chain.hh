036f818e68708e55d5f6979304cb460adcecc82d
Jason Stephenson <jason@sigio.com>
2014-11-11 11:15:58 -0500

    Switch from tabs to spaces in C++ code.
    Signed-off-by: Jason Stephenson <jason@sigio.com>

--- include/chain.hh
+++ include/chain.hh
@@ -40,211 +40,211 @@ namespace markov {
  * not thread safe.
  */
 class chain : public std::map<std::deque<std::string>,
-															std::vector<std::string> > {
-	
+                              std::vector<std::string> > {
+  
 public:
 
-	/*!
-	 * \brief Define a type for the chain prefix.
-	 */
-	typedef std::deque<std::string> prefix;
+  /*!
+   * \brief Define a type for the chain prefix.
+   */
+  typedef std::deque<std::string> prefix;
 
-	/*!
-	 * \brief The constructor.
-	 *
-	 * \param len The length of the prefix used in the chain.
-	 */
-	chain(std::size_t len = 2) :
-		std::map<std::deque<std::string>,
-						 std::vector<std::string> >(),
-		prefix_len(len) {};
+  /*!
+   * \brief The constructor.
+   *
+   * \param len The length of the prefix used in the chain.
+   */
+  chain(std::size_t len = 2) :
+    std::map<std::deque<std::string>,
+             std::vector<std::string> >(),
+    prefix_len(len) {};
 
-	/*!
-	 * \brief Add a string to the chain.
-	 *
-	 * \param s The std::string to add.
-	 */
-	void add(const std::string& s);
+  /*!
+   * \brief Add a string to the chain.
+   *
+   * \param s The std::string to add.
+   */
+  void add(const std::string& s);
 
-	/*!
-	 * \brief Add strings from a input stream to the chain.
-	 *
-	 * \param in The std::istream to read strings from.
-	 * \param resetprefix Whether or not to clear the current prefix
-	 * before adding strings.
-	 */
-	void add(std::istream& in, bool resetprefix = false);
+  /*!
+   * \brief Add strings from a input stream to the chain.
+   *
+   * \param in The std::istream to read strings from.
+   * \param resetprefix Whether or not to clear the current prefix
+   * before adding strings.
+   */
+  void add(std::istream& in, bool resetprefix = false);
 
-	/*!
-	 * \brief Generate scrambled text from the chain starting with a
-	 * given prefix.
-	 *
-	 * \warning This method is not thread safe.
-	 *
-	 * \param s Stream to write the scrambled text to.
-	 * \param nwords The number of words to write.
-	 * \param pref The prefix to start at.
-	 */
-	void generate(std::ostream& s, std::size_t nwords, prefix pref);
+  /*!
+   * \brief Generate scrambled text from the chain starting with a
+   * given prefix.
+   *
+   * \warning This method is not thread safe.
+   *
+   * \param s Stream to write the scrambled text to.
+   * \param nwords The number of words to write.
+   * \param pref The prefix to start at.
+   */
+  void generate(std::ostream& s, std::size_t nwords, prefix pref);
 
-	/*!
-	 * \brief Generate scrambled text from the chain starting with a
-	 * random prefix.
-	 *
-	 * \warning This method is not thread safe.
-	 *
-	 * \param s Stream to write the scrambled text to.
-	 * \param nwords The number of words to write.
-	 */
-	void generate(std::ostream& s, std::size_t nwords);
+  /*!
+   * \brief Generate scrambled text from the chain starting with a
+   * random prefix.
+   *
+   * \warning This method is not thread safe.
+   *
+   * \param s Stream to write the scrambled text to.
+   * \param nwords The number of words to write.
+   */
+  void generate(std::ostream& s, std::size_t nwords);
 
-	/*!
-	 * \brief Output the chain to a stream in a format that can easily
-	 * be read back in.
-	 *
-	 * The current data of the instance is written to the stream in a
-	 * simple text format.  There is one line per chain entry.  Each
-	 * line consists of the prefix followed by the suffix chain.  The
-	 * prefix is separated from the suffix by the sequence of space,
-	 * colon, space.  The prefix members are separated from each other
-	 * by a space, as are the suffix members.  An example output file,
-	 * created with the default prefix length of 2, of Hamlet's famous
-	 * soliloquy follows:
-	 *
-	 * \verbinclude hamlet.txt
-	 *
-	 * Another example (taken from http://en.wikipedia.org/wiki/Markov_chain)
-	 * is the dietary habits of a creature who eats only grapes, cheese
-	 * or lettuce, and whose dietary habits conform to the following
-	 * rules:
-	 *
-	 * \li It eats exactly once a day.
-	 *
-	 * \li If it ate cheese today, tomorrow it will eat lettuce or
-	 * grapes with equal probability.
-	 *
-	 * \li If it ate grapes today, tomorrow it will eat grapes with
-	 * probability 1/10, cheese with probability 4/10 and lettuce with
-	 * probability 5/10.
-	 *
-	 * \li If it ate lettuce today, it will not eat lettuce again
-	 * tomorrow but will eat grapes with probability 4/10 or cheese with
-	 * probability 6/10.
-	 *
-	 * The above could be modeled in a markov::chain, and when written
-	 * to a file would look like this:
-	 *
-	 * \verbinclude diet.txt
-	 *
-	 * \param s The stream to write to.
-	 */
-	void write(std::ostream& s);
+  /*!
+   * \brief Output the chain to a stream in a format that can easily
+   * be read back in.
+   *
+   * The current data of the instance is written to the stream in a
+   * simple text format.  There is one line per chain entry.  Each
+   * line consists of the prefix followed by the suffix chain.  The
+   * prefix is separated from the suffix by the sequence of space,
+   * colon, space.  The prefix members are separated from each other
+   * by a space, as are the suffix members.  An example output file,
+   * created with the default prefix length of 2, of Hamlet's famous
+   * soliloquy follows:
+   *
+   * \verbinclude hamlet.txt
+   *
+   * Another example (taken from http://en.wikipedia.org/wiki/Markov_chain)
+   * is the dietary habits of a creature who eats only grapes, cheese
+   * or lettuce, and whose dietary habits conform to the following
+   * rules:
+   *
+   * \li It eats exactly once a day.
+   *
+   * \li If it ate cheese today, tomorrow it will eat lettuce or
+   * grapes with equal probability.
+   *
+   * \li If it ate grapes today, tomorrow it will eat grapes with
+   * probability 1/10, cheese with probability 4/10 and lettuce with
+   * probability 5/10.
+   *
+   * \li If it ate lettuce today, it will not eat lettuce again
+   * tomorrow but will eat grapes with probability 4/10 or cheese with
+   * probability 6/10.
+   *
+   * The above could be modeled in a markov::chain, and when written
+   * to a file would look like this:
+   *
+   * \verbinclude diet.txt
+   *
+   * \param s The stream to write to.
+   */
+  void write(std::ostream& s);
 
-	/*!
-	 * \brief Read a chain in from a stream that was previously written
-	 * with the write method.
-	 *
-	 * \param s The stream to read from.
-	 * \see write
-	 */
-	void read(std::istream& s);
+  /*!
+   * \brief Read a chain in from a stream that was previously written
+   * with the write method.
+   *
+   * \param s The stream to read from.
+   * \see write
+   */
+  void read(std::istream& s);
 
-	/*!
-	 * \brief Return the value of the current_prefix member.
-	 */
-	prefix currentPrefix();
+  /*!
+   * \brief Return the value of the current_prefix member.
+   */
+  prefix currentPrefix();
 
-	/*!
-	 * \brief Return a random prefix from the chain.
-	 */
-	prefix randomPrefix();
+  /*!
+   * \brief Return a random prefix from the chain.
+   */
+  prefix randomPrefix();
 
-	/*!
-	 * \brief Check if a chain instance has a prefix matching the
-	 * argument.
-	 *
-	 * \param pref The prefix to check.
-	 * \return True if the argument appears as a prefix in the instance,
-	 * false if not.
-	 */
-	bool isValidPrefix(prefix pref);
+  /*!
+   * \brief Check if a chain instance has a prefix matching the
+   * argument.
+   *
+   * \param pref The prefix to check.
+   * \return True if the argument appears as a prefix in the instance,
+   * false if not.
+   */
+  bool isValidPrefix(prefix pref);
 
-	/*!
-	 * \brief Return the value of the prefix length member.
-	 */
-	std::size_t prefixLength();
+  /*!
+   * \brief Return the value of the prefix length member.
+   */
+  std::size_t prefixLength();
 
-	/*!
-	 * \brief Check if the class has seeded the random number generator.
-	 *
-	 * This method is generally used by the implementation methods of
-	 * this class.  It is available if for some reason you want to know
-	 * if the pseudo-random number generator has been seeded.
-	 *
-	 * \return True if the prng has been seeded, false if not.
-	 */
-	static bool isSeeded();
+  /*!
+   * \brief Check if the class has seeded the random number generator.
+   *
+   * This method is generally used by the implementation methods of
+   * this class.  It is available if for some reason you want to know
+   * if the pseudo-random number generator has been seeded.
+   *
+   * \return True if the prng has been seeded, false if not.
+   */
+  static bool isSeeded();
 
-	/*!
-	 * \brief Seed the class wide random number generator.
-	 *
-	 * Client code generally does not need to use this class method.
-	 * The other members will call it if necessary.  It is there,
-	 * however, if you do want to change the seed on the pseudo-random
-	 * number generator for some reason.  Keep in mind that it doesn't
-	 * let you pick the seed.
-	 *
-	 * Also, if the isSeeded static method would return true, this
-	 * method will not actually do anything unless the force argument is
-	 * true.  The default is false.
-	 *
-	 * \warning This method is not thread safe.
-	 *
-	 * \param force If true, reseed the generator, even if it has
-	 * already been seeded.
-	 */
-	static void seed(bool force = false);
+  /*!
+   * \brief Seed the class wide random number generator.
+   *
+   * Client code generally does not need to use this class method.
+   * The other members will call it if necessary.  It is there,
+   * however, if you do want to change the seed on the pseudo-random
+   * number generator for some reason.  Keep in mind that it doesn't
+   * let you pick the seed.
+   *
+   * Also, if the isSeeded static method would return true, this
+   * method will not actually do anything unless the force argument is
+   * true.  The default is false.
+   *
+   * \warning This method is not thread safe.
+   *
+   * \param force If true, reseed the generator, even if it has
+   * already been seeded.
+   */
+  static void seed(bool force = false);
 
 protected:
 
-	/*!
-	 * \brief Set the current prefix member to the argument.
-	 *
-	 * Client code generally has no business changing the current prefix
-	 * value.  The public methods will change it as necessary.  However,
-	 * a subclass has no way to change the current prefix value and may
-	 * need to, particularly when reimplement the read method.  Since
-	 * this method checks the validity of the prefix parameter, it
-	 * should be used after the new prefix has been added to the
-	 * instance.
-	 *
-	 * \param pref The new value for the current prefix.
-	 * \return The value of current prefix after calling this method.
-	 */
-	prefix currentPrefix(prefix pref);
+  /*!
+   * \brief Set the current prefix member to the argument.
+   *
+   * Client code generally has no business changing the current prefix
+   * value.  The public methods will change it as necessary.  However,
+   * a subclass has no way to change the current prefix value and may
+   * need to, particularly when reimplement the read method.  Since
+   * this method checks the validity of the prefix parameter, it
+   * should be used after the new prefix has been added to the
+   * instance.
+   *
+   * \param pref The new value for the current prefix.
+   * \return The value of current prefix after calling this method.
+   */
+  prefix currentPrefix(prefix pref);
 
-	/*!
-	 * \brief Change the value of the object's prefix length member.
-	 *
-	 * This method should not be needed by client code.  Subclasses that
-	 * reimplement the read method, perhaps to support a different file
-	 * storage format, may need to call this method to set the prefix
-	 * length.
-	 *
-	 * This method has the side effect of clearing the instance as well
-	 * as the instance's current prefix value.  Therefore, it should be
-	 * called before setting any values in a read.
-	 *
-	 * \param len The new length for the prefix length.
-	 * \return The new length for the prefix length, if successful.
-	 */
-	std::size_t prefixLength(std::size_t len);
+  /*!
+   * \brief Change the value of the object's prefix length member.
+   *
+   * This method should not be needed by client code.  Subclasses that
+   * reimplement the read method, perhaps to support a different file
+   * storage format, may need to call this method to set the prefix
+   * length.
+   *
+   * This method has the side effect of clearing the instance as well
+   * as the instance's current prefix value.  Therefore, it should be
+   * called before setting any values in a read.
+   *
+   * \param len The new length for the prefix length.
+   * \return The new length for the prefix length, if successful.
+   */
+  std::size_t prefixLength(std::size_t len);
 
 private:
-	prefix current_prefix;
-	std::size_t prefix_len;
-	static bool is_seeded;
-	bool parseLine(std::string line);
+  prefix current_prefix;
+  std::size_t prefix_len;
+  static bool is_seeded;
+  bool parseLine(std::string line);
 
 };
 
--- src/chain.cc
+++ src/chain.cc
@@ -32,191 +32,191 @@ bool chain::is_seeded = false;
 
 static std::size_t getSeed(void)
 {
-	std::size_t seed = 0;
+  std::size_t seed = 0;
 #ifdef HAVE_RANDOM_DEVICE
-	std::ifstream in(RANDOM_DEVICE, std::ios::in);
-	in.read((char *)&seed, sizeof(seed));
-	in.close();
+  std::ifstream in(RANDOM_DEVICE, std::ios::in);
+  in.read((char *)&seed, sizeof(seed));
+  in.close();
 #else
-	(void)std::time((std::time_t *)&seed);
+  (void)std::time((std::time_t *)&seed);
 #endif
-	return seed;
+  return seed;
 }
 
 void chain::add(const std::string& s) {
-	if (this->current_prefix.size() == this->prefix_len) {
-		(*this)[this->current_prefix].push_back(s);
-		this->current_prefix.pop_front();
-	}
-	this->current_prefix.push_back(s);
+  if (this->current_prefix.size() == this->prefix_len) {
+    (*this)[this->current_prefix].push_back(s);
+    this->current_prefix.pop_front();
+  }
+  this->current_prefix.push_back(s);
 }
 
 void chain::add(std::istream& in, bool resetprefix) {
-	std::string buf;
-	if (resetprefix)
-		this->current_prefix.clear();
-	while(in >> buf)
-		this->add(buf);
+  std::string buf;
+  if (resetprefix)
+    this->current_prefix.clear();
+  while(in >> buf)
+    this->add(buf);
 }
 
 void chain::generate(std::ostream& s, std::size_t nwords, prefix pref) {
-	if (this->isValidPrefix(pref))
-		this->current_prefix = pref;
-	else
-		this->current_prefix = this->randomPrefix();
+  if (this->isValidPrefix(pref))
+    this->current_prefix = pref;
+  else
+    this->current_prefix = this->randomPrefix();
 
-	if (!chain::is_seeded)
-		chain::seed();
+  if (!chain::is_seeded)
+    chain::seed();
 
-	std::size_t i;
-	for (i = 0; i < this->prefix_len; i++)
-		s << this->current_prefix.at(i) << ' ';
+  std::size_t i;
+  for (i = 0; i < this->prefix_len; i++)
+    s << this->current_prefix.at(i) << ' ';
 
-	for (; i < nwords; i++) {
-		std::vector<std::string>& suf = (*this)[current_prefix];
-		const std::string& w = suf[random() % suf.size()];
-		s << w << ' ';
-		// To avoid a SIGFPE when we get the last entry from the original
-		// input:
-		pref = this->current_prefix;
-		pref.pop_front();
-		pref.push_back(w);
-		if (this->isValidPrefix(pref))
-			this->current_prefix = pref;
-		else
-			break;
-	}
+  for (; i < nwords; i++) {
+    std::vector<std::string>& suf = (*this)[current_prefix];
+    const std::string& w = suf[random() % suf.size()];
+    s << w << ' ';
+    // To avoid a SIGFPE when we get the last entry from the original
+    // input:
+    pref = this->current_prefix;
+    pref.pop_front();
+    pref.push_back(w);
+    if (this->isValidPrefix(pref))
+      this->current_prefix = pref;
+    else
+      break;
+  }
 
-	s << std::endl;
+  s << std::endl;
 }
 
 void chain::generate(std::ostream& s, std::size_t nwords) {
-	prefix start = this->randomPrefix();
-	this->generate(s, nwords, start);
+  prefix start = this->randomPrefix();
+  this->generate(s, nwords, start);
 }
 
 void chain::write(std::ostream& s) {
-	for (const_iterator it = this->begin(); it != this->end(); it++) {
-		prefix pref = it->first;
-		std::vector<std::string> suf = it->second;
+  for (const_iterator it = this->begin(); it != this->end(); it++) {
+    prefix pref = it->first;
+    std::vector<std::string> suf = it->second;
 
-		for (prefix::const_iterator pit = pref.begin(); pit != pref.end(); pit++)
-			s << *pit << ' ';
-		s << ": ";
+    for (prefix::const_iterator pit = pref.begin(); pit != pref.end(); pit++)
+      s << *pit << ' ';
+    s << ": ";
 
-		std::vector<std::string>::const_iterator vit = suf.begin();
-		while (vit != suf.end()) {
-			s << *vit++;
-			if (vit != suf.end())
-				s << ' ';
-			else
-				s << std::endl;
-		}
-	}
+    std::vector<std::string>::const_iterator vit = suf.begin();
+    while (vit != suf.end()) {
+      s << *vit++;
+      if (vit != suf.end())
+        s << ' ';
+      else
+        s << std::endl;
+    }
+  }
 }
 
 void chain::read(std::istream &s) {
-	this->clear();
-	this->current_prefix.clear();
-	this->prefix_len = 0;
-	while (s.good()) {
-		std::string line;
-		std::getline(s, line);
-		if (s.good())
-			this->parseLine(line);
-	}
+  this->clear();
+  this->current_prefix.clear();
+  this->prefix_len = 0;
+  while (s.good()) {
+    std::string line;
+    std::getline(s, line);
+    if (s.good())
+      this->parseLine(line);
+  }
 }
 
 bool chain::parseLine(std::string line) {
-	bool success = false;
-	std::size_t cpos = line.find(" : ");
-	if (cpos != std::string::npos) {
-		prefix temp;
-		std::string pstr = line.substr(0, cpos);
-		std::size_t spos = 0;
+  bool success = false;
+  std::size_t cpos = line.find(" : ");
+  if (cpos != std::string::npos) {
+    prefix temp;
+    std::string pstr = line.substr(0, cpos);
+    std::size_t spos = 0;
 
-		do {
-			spos = pstr.find(' ');
-			temp.push_back(pstr.substr(0, spos));
-			if (spos != std::string::npos)
-				pstr = pstr.substr(spos + 1);
-		} while (spos != std::string::npos);
+    do {
+      spos = pstr.find(' ');
+      temp.push_back(pstr.substr(0, spos));
+      if (spos != std::string::npos)
+        pstr = pstr.substr(spos + 1);
+    } while (spos != std::string::npos);
 
-		if (this->prefix_len == 0)
-			this->prefix_len = temp.size();
+    if (this->prefix_len == 0)
+      this->prefix_len = temp.size();
 
-		if (temp.size() == this->prefix_len) {
-			this->current_prefix = temp;
-			pstr = line.substr(cpos + 3);
+    if (temp.size() == this->prefix_len) {
+      this->current_prefix = temp;
+      pstr = line.substr(cpos + 3);
 
-			do {
-				spos = pstr.find(' ');
-				(*this)[temp].push_back(pstr.substr(0, spos));
-				if (spos != std::string::npos)
-					pstr = pstr.substr(spos + 1);
-			} while (spos != std::string::npos);
+      do {
+        spos = pstr.find(' ');
+        (*this)[temp].push_back(pstr.substr(0, spos));
+        if (spos != std::string::npos)
+          pstr = pstr.substr(spos + 1);
+      } while (spos != std::string::npos);
 
-			success = true;
-		}
-	}
-	return success;
+      success = true;
+    }
+  }
+  return success;
 }
 
 
 chain::prefix chain::currentPrefix() {
-	return this->current_prefix;
+  return this->current_prefix;
 }
 
 chain::prefix chain::currentPrefix(prefix pref) {
-	if (this->isValidPrefix(pref))
-		this->current_prefix = pref;
-	return this->current_prefix;
+  if (this->isValidPrefix(pref))
+    this->current_prefix = pref;
+  return this->current_prefix;
 }
 
 chain::prefix chain::randomPrefix() {
-	prefix pref;
+  prefix pref;
 
-	if (!chain::is_seeded)
-		chain::seed();
+  if (!chain::is_seeded)
+    chain::seed();
 
-	std::size_t i = random() % this->size();
-	std::size_t j = 0;
-	for (const_iterator it = this->begin(); it != this->end() && j <= i;
-			 it++, j++)
-		pref = it->first;
+  std::size_t i = random() % this->size();
+  std::size_t j = 0;
+  for (const_iterator it = this->begin(); it != this->end() && j <= i;
+       it++, j++)
+    pref = it->first;
 
-	return pref;
+  return pref;
 }
 
 bool chain::isValidPrefix(prefix pref) {
-	bool isValid = true;
-	iterator it = this->find(pref);
-	if (it == this->end())
-		isValid = false;
+  bool isValid = true;
+  iterator it = this->find(pref);
+  if (it == this->end())
+    isValid = false;
 
-	return isValid;
+  return isValid;
 }
 
 std::size_t chain::prefixLength() {
-	return this->prefix_len;
+  return this->prefix_len;
 }
 
 std::size_t chain::prefixLength(std::size_t len) {
-	this->clear();
-	this->current_prefix.clear();
-	this->prefix_len = len;
-	return this->prefix_len;
+  this->clear();
+  this->current_prefix.clear();
+  this->prefix_len = len;
+  return this->prefix_len;
 }
 
 bool chain::isSeeded() {
-	return chain::is_seeded;
+  return chain::is_seeded;
 }
 
 void chain::seed(bool force) {
-	if (force || !chain::is_seeded) {
-		srandom(getSeed());
-		chain::is_seeded = true;
-	}
+  if (force || !chain::is_seeded) {
+    srandom(getSeed());
+    chain::is_seeded = true;
+  }
 }
 
 }
